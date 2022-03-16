4a1c04f716f10c6af1f22edf61fb94bbd4cfcb0e
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2022-03-16 10:42:05 -0300

    first commit

---
+++ JS/index.js
@@ -0,0 +1,27 @@
+//Soft Scroll
+
+$(document).ready(function () {
+   $("#logo").click(function () { 
+       $("html, body").animate({
+           scrollTop: $("#banner").offset().top}, 500);
+    });
+
+    $("#home").click(function () { 
+        $("html, body").animate({
+            scrollTop: $("#cardsTitle").offset().top}, 500);
+     });
+
+     
+
+//function filtrar empresas
+
+    $("#fieldFinder").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("h4").filter(function() {
+            $(this).parents(".card").toggle( $(this).text().toLowerCase().indexOf(value) > -1 )
+        });
+    });
+});
+
+//effect fade aliado ao scroll
+
